cf5719d50055c814b17abcc007f7997c3b5f5e3d
Mohammad Shakhatreh <hmodaa.sha@hotmail.com>
2020-08-16 12:24:24 +0300

    Add netlify.toml

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.BASE_URL || 'http://localhost:8080',
   },
   /*
    ** vuetify module configuration
